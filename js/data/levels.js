964ebefe96b726ea097b93fe3aa6aaf788ab0d3a
Rimantas <belauzas@yahoo.com>
2019-12-08 14:49:51 +0200

    Map update

--- js/data/levels.js
+++ js/data/levels.js
@@ -1,3 +1,72 @@
 export default [
-    
+    {
+        level: 10,
+        top: -5,
+        left: 66,
+        cityName: 'Vilnius',
+        tree: 2
+    },
+    {
+        level: 9,
+        top: 6,
+        left: 44,
+        cityName: 'Kaunas',
+        tree: 3
+    },
+    {
+        level: 8,
+        top: 17,
+        left: 20,
+        cityName: 'Klaipėda',
+        tree: 1
+    },
+    {
+        level: 7,
+        top: 32,
+        left: 34,
+        cityName: 'Šiauliai',
+        tree: 2
+    },
+    {
+        level: 6,
+        top: 37,
+        left: 67,
+        cityName: 'Panevėžys',
+        tree: 3
+    },
+    {
+        level: 5,
+        top: 50,
+        left: 75,
+        cityName: 'Marijampolė',
+        tree: 1
+    },
+    {
+        level: 4,
+        top: 57,
+        left: 43,
+        cityName: 'Alytus',
+        tree: 2
+    },
+    {
+        level: 3,
+        top: 63,
+        left: 15,
+        cityName: 'Telšiai',
+        tree: 3
+    },
+    {
+        level: 2,
+        top: 77,
+        left: 24,
+        cityName: 'Utena',
+        tree: 1
+    },
+    {
+        level: 1,
+        top: 84,
+        left: 53,
+        cityName: 'Tauragė',
+        tree: 2
+    }
 ]
--- js/objects/MapScreen.js
+++ js/objects/MapScreen.js
@@ -1,17 +1,73 @@
+import levels from '../data/levels.js';
+
 class MapScreen {
     constructor ( DOM ) {
         this.DOM = null;
+        this.DOMtop = null;
+        this.DOMlevelContainer = null;
+        this.DOMsanta = null;
+        this.levels = null;
+        this.userLevel = 1;
+        this.santaCurrentCity = 0;
+        this.santaMoving = false;
 
         this.init( DOM );
     }
 
     init( DOM ) {
-        DOM.insertAdjacentHTML('beforeend', `
+
+        DOM.insertAdjacentHTML('afterbegin', `
             <div class="map">
-                MAP
+                <div class="top"></div>
+                <div class="levels">
+                    <div class="container">
+                        <img class="track" src="./img/map-track.png">
+                        <div class="santa"></div>
+                        <div class="level start"></div>
+                    </div>
+                </div>
             </div>
         `)
         this.DOM = document.querySelector('.map');
+        this.DOMtop = this.DOM.querySelector('.top');
+        this.DOMlevelContainer = this.DOM.querySelector('.levels > .container');
+        this.DOMsanta = this.DOMlevelContainer.querySelector('.santa');
+        this.renderLevelMap();
+
+        this.DOMlevels = this.DOMlevelContainer.querySelectorAll('.level.city');
+        for ( let i=0; i<this.DOMlevels.length; i++ ) {
+            const level = this.DOMlevels[i];
+            level.addEventListener('click', () => {
+                this.moveSantaToCity(10-i);
+            })
+        }
+
+    }
+
+    moveSantaToCity( cityIndex ) {
+        if ( !this.santaMoving ) {
+            this.santaMoving = !this.santaMoving;
+            const city = levels.filter( c => c.level === cityIndex )[0];
+            this.DOMsanta.style.top = city.top + '%';
+            this.DOMsanta.style.left = city.left + '%';
+            
+            setTimeout(() => {
+                this.santaMoving = !this.santaMoving;
+            }, 1000)
+        }
+    }
+
+    renderLevelMap() {
+        let HTML = '';
+        for ( let i=0; i<levels.length; i++ ) {
+            const level = levels[i];
+            HTML += `<div class="level city"
+                        style="top: ${level.top}%;
+                                left: ${level.left}%;">
+                        <div class="city-name">${level.cityName}</div>
+                    </div>`;
+        }
+        this.DOMlevelContainer.insertAdjacentHTML('beforeend', HTML);
     }
 }
 
